5b84b0faec738953d6880aba71b8a1cc1479d015
x74z <166341818+x74z@users.noreply.github.com>
2025-01-03 15:33:30 +0000

    Add form for location search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import getApiKey from "./api-key";
+import getE from "./api-key";
 import "./styles.css";
 
 function proccessWeatherDataForDate(data) {
@@ -17,15 +17,30 @@ function proccessWeatherDataForDate(data) {
     tempmax: data.days[date].tempmax,
     tempmin: data.days[date].tempmin,
     uvindex: data.days[date].uvindex,
+    sunrise: data.days[date].sunrise,
+    sunset: data.days[date].sunset,
+    visibility: data.days[date].visibility,
   };
   console.log(valuableData);
   return valuableData;
 }
-async function getWeatherDataJson() {
-  const responseData = await fetch( `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/united%20states?unitGroup=us&key=${getApiKey()}&contentType=json`);
+function formatLocationForUrl(location){
+  return location.replaceAll(' ', '%20');
+}
+async function getWeatherDataJson(locationInput) {
+  const location = formatLocationForUrl(locationInput);
+  const responseData = await fetch( `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${getE()}&contentType=json`);
   const dataInJson = await responseData.json();
 
   console.log(dataInJson);
   return dataInJson;
 }
-getWeatherDataJson().then(proccessWeatherDataForDate)
+
+(()=>{
+  const weatherForm = document.querySelector(".js-weather-location-form")
+  weatherForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    getWeatherDataJson(weatherForm.location.value).then(proccessWeatherDataForDate)
+
+  })
+})();
