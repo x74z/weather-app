12095da5fb23b8723933aecdd660b58994dbaf94
x74z <166341818+x74z@users.noreply.github.com>
2025-01-05 23:13:17 +0000

    Make     error appear on screen

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import "./styles.css";
 import { addDataToDom } from "./add-data-to-dom.js";
 import { getWeatherDataJson, proccessWeatherDataForDate } from "./getting-weather-data.js";
 
-
-
-(()=>{
-  const weatherForm = document.querySelector(".js-weather-location-form")
-  weatherForm.addEventListener("submit", (e)=>{
+(() => {
+  const weatherForm = document.querySelector(".js-weather-location-form");
+  weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    getWeatherDataJson(weatherForm.location.value).then(proccessWeatherDataForDate)
+
+    getWeatherDataJson(weatherForm.location.value)
+      .then(proccessWeatherDataForDate)
       .then(addDataToDom)
-  })
+
+
+      .catch((error) => {
+        // Show the error on this element, so it doesnt break
+        document.querySelector(".js-weather-condition").textContent =
+          error.message;
+      });
+  });
 })();
