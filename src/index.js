ddfb44849b88fe6625ceeb680dcbf5c7ea82c881
x74z <166341818+x74z@users.noreply.github.com>
2025-01-05 17:02:01 +0000

    Move weather fetching to a new file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
-import getE from "./api-key";
 import "./styles.css";
+import { addDataToDom } from "./add-data-to-dom.js";
+import { getWeatherDataJson, proccessWeatherDataForDate } from "./getting-weather-data.js";
 
-function proccessWeatherDataForDate(data) {
-  // function getIndexOfDate(date){ }
-  // const processedDate = getIndexOfDate(date);
 
-  // 0 is today, 1 tomorrow...
-  const date = 0;
-
-  const valuableData = {
-    conditions: data.days[date].conditions,
-    icon: data.days[date].icon,
-    feelslike: data.days[date].feelslike,
-    precipitation: data.days[date].precip,
-    temp: data.days[date].temp,
-    tempmax: data.days[date].tempmax,
-    tempmin: data.days[date].tempmin,
-    uvindex: data.days[date].uvindex,
-    sunrise: data.days[date].sunrise,
-    sunset: data.days[date].sunset,
-    visibility: data.days[date].visibility,
-  };
-  console.log(valuableData);
-  return valuableData;
-}
-function formatLocationForUrl(location){
-  return location.replaceAll(' ', '%20');
-}
-async function getWeatherDataJson(locationInput) {
-  const location = formatLocationForUrl(locationInput);
-  const responseData = await fetch( `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${getE()}&contentType=json`);
-  const dataInJson = await responseData.json();
-
-  console.log(dataInJson);
-  return dataInJson;
-}
 
 (()=>{
   const weatherForm = document.querySelector(".js-weather-location-form")
   weatherForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     getWeatherDataJson(weatherForm.location.value).then(proccessWeatherDataForDate)
-
+      .then(addDataToDom)
   })
 })();
